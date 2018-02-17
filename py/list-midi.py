41c352fe13191c473154dbf5f61a7fd57e6dbe8a
James Hong <james.hong@cs.stanford.edu>
2018-02-16 16:31:36 -0800

    Write output back to the mixer board

--- py/list-midi.py
+++ py/list-midi.py
@@ -4,6 +4,12 @@ import rtmidi
 
 midiin = rtmidi.RtMidiIn()
 
-print('Port\tName')
+print('Port\tName (IN)')
 for i in range(midiin.getPortCount()):
     print('{}\t{}'.format(i, midiin.getPortName(i)))
+
+midiout = rtmidi.RtMidiIn()
+
+print('Port\tName (OUT)')
+for i in range(midiout.getPortCount()):
+    print('{}\t{}'.format(i, midiout.getPortName(i)))
--- py/mm-interactive.py
+++ py/mm-interactive.py
@@ -25,9 +25,9 @@ DEFAULT_MIDI_CTRL_DROP_BUTTON = 73
 MIDI_CTRL_SLIDER_MAX = 127
 
 
+# Capture all data required to output to the terminal and to the mmapped file
 AppConfig = namedtuple('AppConfig', ['window', 'midi_port', 'mm', 'f',
-                                     'control_file', 'max_mbps', 'min_mbps',
-                                     'no_ui'])
+                                     'control_file', 'max_mbps', 'min_mbps'])
 
 
 def get_args():
@@ -102,10 +102,104 @@ def write_to_mm_region(conf, mbps, link_on):
 
 def cause_temporary_outage(conf, mbps):
     write_to_mm_region(conf, mbps, False)
-    if not conf.no_ui:
+    if conf.window:
         refresh_window(conf, mbps, False)
         curses.beep()
     time.sleep(OUTAGE_LENGTH_IN_MS / 1000.0)
+
+
+def keyboard_loop(conf):
+    assert conf.window, 'Using keyboard input requires curses UI'
+    curr_bw = conf.max_mbps
+    write_to_mm_region(conf, curr_bw, True)
+    refresh_window(conf, curr_bw, True)
+    while True:
+        k = conf.window.getch()
+        if k == ord('\n') or k == curses.KEY_ENTER:
+            cause_temporary_outage(conf, curr_bw)
+        elif k == curses.KEY_UP:
+            curr_bw = min(curr_bw + 0.1, conf.max_mbps)
+        elif k == curses.KEY_DOWN:
+            curr_bw = max(conf.min_mbps, curr_bw - 1)
+        else:
+            # Ignored input
+            continue
+
+        # Update the display
+        write_to_mm_region(conf, curr_bw, True)
+        refresh_window(conf, curr_bw, True)
+
+
+def midi_loop(conf, midi_ctrl_bw, midi_ctrl_drop):
+    midiin = rtmidi.RtMidiIn()
+    midiin.openPort(conf.midi_port)
+
+    midiout = rtmidi.RtMidiOut()
+    midiout.openPort(conf.midi_port)
+
+    slider_increment = (float(conf.max_mbps - conf.min_mbps) /
+                        MIDI_CTRL_SLIDER_MAX)
+
+    curr_bw = conf.max_mbps
+    write_to_mm_region(conf, curr_bw, True)
+    if conf.window:
+        refresh_window(conf, curr_bw, True)
+
+    # Turn the drop light off
+    midiout.sendMessage(rtmidi.MidiMessage.controllerEvent(
+                        1, midi_ctrl_drop, 0))
+    # Set initial slider position to maximum
+    midiout.sendMessage(rtmidi.MidiMessage.controllerEvent(
+                        1, midi_ctrl_bw, 127))
+
+    while True:
+        m = midiin.getMessage(250)
+        if not m:
+            continue
+
+        # print_midi_message(m)
+        if not m.isController():
+            continue
+
+        ctrl_no = m.getControllerNumber()
+        if ctrl_no == midi_ctrl_bw:
+            slider_val = m.getControllerValue()
+            assert(slider_val >= 0)
+            new_bw = slider_val * slider_increment + conf.min_mbps
+            new_bw = min(conf.max_mbps, new_bw)
+            new_bw = max(conf.min_mbps, new_bw)
+            curr_bw = new_bw
+
+        elif ctrl_no == midi_ctrl_drop:
+            slider_val = int((curr_bw - conf.min_mbps) / slider_increment)
+            slider_val = min(slider_val, 127)
+            slider_val = max(slider_val, 0)
+
+            # Set the slider to 0
+            midiout.sendMessage(rtmidi.MidiMessage.controllerEvent(
+                                1, midi_ctrl_bw, 0))
+
+            cause_temporary_outage(conf, curr_bw)
+
+            # Turn off the drop light
+            midiout.sendMessage(rtmidi.MidiMessage.controllerEvent(
+                                1, midi_ctrl_drop, 0))
+            # Set the slider back
+            midiout.sendMessage(rtmidi.MidiMessage.controllerEvent(
+                                1, midi_ctrl_bw, slider_val))
+
+        write_to_mm_region(conf, curr_bw, True)
+        if conf.window:
+            refresh_window(conf, curr_bw, True)
+
+
+def init_window():
+    window = curses.initscr()
+    window.keypad(True)
+    window.clear()
+    curses.noecho()
+    curses.cbreak()
+    return window
 
 
 def main(args):
@@ -118,8 +212,6 @@ def main(args):
     min_mbps = args.min
     max_mbps = args.max
 
-    curr_bw = max_mbps
-
     with open(control_file, 'wb+') as f:
         mmap_len = 2 * SIZEOF_UINT64_T
         f.write(bytes([0] * mmap_len))
@@ -127,72 +219,13 @@ def main(args):
         mm = mmap.mmap(f.fileno(), mmap_len, prot=mmap.PROT_WRITE)
 
         if not no_ui:
-            window = curses.initscr()
-            window.keypad(True)
-            window.clear()
-            curses.noecho()
-            curses.cbreak()
+            window = init_window()
         else:
             window = None
 
         conf = AppConfig(window=window, midi_port=midi_port, mm=mm, f=f,
-                         control_file=control_file, no_ui=no_ui,
+                         control_file=control_file,
                          max_mbps=max_mbps, min_mbps=min_mbps)
-
-        write_to_mm_region(conf, curr_bw, True)
-        if not no_ui:
-            refresh_window(conf, curr_bw, True)
-
-        def keyboard_loop(conf):
-            assert not conf.no_ui, 'Using keyboard input requires curses'
-            nonlocal curr_bw
-            while True:
-                k = window.getch()
-                if k == ord('\n') or k == curses.KEY_ENTER:
-                    cause_temporary_outage(conf, curr_bw)
-                elif k == curses.KEY_UP:
-                    curr_bw = min(curr_bw + 0.1, conf.max_mbps)
-                elif k == curses.KEY_DOWN:
-                    curr_bw = max(conf.min_mbps, curr_bw - 1)
-                else:
-                    # Ignored input
-                    continue
-
-                # Update the display
-                write_to_mm_region(conf, curr_bw, True)
-                refresh_window(conf, curr_bw, True)
-
-        def midi_loop(conf, midi_ctrl_bw, midi_ctrl_drop):
-            nonlocal curr_bw
-            midiin = rtmidi.RtMidiIn()
-            midiin.openPort(conf.midi_port)
-
-            slider_increment = (float(conf.max_mbps - conf.min_mbps) /
-                                MIDI_CTRL_SLIDER_MAX)
-
-            while True:
-                m = midiin.getMessage(250)
-                if not m:
-                    continue
-
-                # print_midi_message(m)
-                if not m.isController():
-                    continue
-
-                ctrl_no = m.getControllerNumber()
-                if ctrl_no == midi_ctrl_bw:
-                    slider_val = m.getControllerValue()
-                    assert(slider_val >= 0)
-                    new_bw = slider_val * slider_increment + conf.min_mbps
-                    new_bw = min(conf.max_mbps, new_bw)
-                    new_bw = max(conf.min_mbps, new_bw)
-                    curr_bw = new_bw
-                elif ctrl_no == midi_ctrl_drop:
-                    cause_temporary_outage(conf, curr_bw)
-
-                write_to_mm_region(conf, curr_bw, True)
-                if not conf.no_ui:
-                    refresh_window(conf, curr_bw, True)
 
         if midi_port is not None:
             midi_loop(conf, midi_ctrl_bw, midi_ctrl_drop)
